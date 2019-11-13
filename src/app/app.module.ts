9facff963769f62f95c19908a3cbc42ff882c67f
Prv <npraju999@gmail.com>
2019-11-13 01:17:35 -0500

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SwiftComponent } from './swift/swift.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SwiftComponent
   ],
   imports: [
     BrowserModule,
