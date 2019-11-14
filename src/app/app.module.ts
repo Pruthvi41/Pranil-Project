c5eea2848fe1a3cc7fdf4ce603a0286e058843dd
Prv <npraju999@gmail.com>
2019-11-14 18:31:15 -0500

    14-11-2019 : login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SwiftComponent } from './swift/swift.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SwiftComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
