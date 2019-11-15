08e9b883f5e7585da462aad25e77a773d47abc16
Prv <npraju999@gmail.com>
2019-11-15 16:19:54 -0500

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,27 @@ import { AppComponent } from './app.component';
 import { SwiftComponent } from './swift/swift.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
+
+//const routes: Routes = [
+  //{ path: '', redirectTo: 'login', pathMatch: 'full'},
+  //{ path: 'login', component: LoginFormComponent },
+  //{ path: 'register', component: RegistrationFormComponent },
+  //{ path: 'todo', component: TodoAppComponent, canActivate: [AuthGuard] },
+  //{ path: 'logout', component: LogoutComponent },
+  //{ path: 'detail/:id', component: TodoDetailComponent, canActivate: [AuthGuard] },
+  //{ path: '**', component: PageNotFoundComponent }
+//]
 
 @NgModule({
   declarations: [
     AppComponent,
     SwiftComponent,
     FooterComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    PageNotFoundComponentComponent
   ],
   imports: [
     BrowserModule,
