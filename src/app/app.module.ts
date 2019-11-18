7141f62186dae1da7618d7dc3fb566d70554b9f0
Prv <npraju999@gmail.com>
2019-11-17 22:15:07 -0500

    17-11-2019-Complete login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,15 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
+import { SelectionComponent } from './selection/selection.component';
+import { ScheduleComponent } from './schedule/schedule.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: '', redirectTo: 'selection', pathMatch: 'full'},
+  { path: 'selection', component: SelectionComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'schedule', component: ScheduleComponent },
   { path: 'swift', redirectTo: 'login' },
   { path: '**', component: PageNotFoundComponentComponent }
 ]
@@ -25,7 +29,9 @@ const routes: Routes = [
     FooterComponent,
     LoginComponent,
     RegisterComponent,
-    PageNotFoundComponentComponent
+    PageNotFoundComponentComponent,
+    SelectionComponent,
+    ScheduleComponent
   ],
   imports: [
     BrowserModule,
